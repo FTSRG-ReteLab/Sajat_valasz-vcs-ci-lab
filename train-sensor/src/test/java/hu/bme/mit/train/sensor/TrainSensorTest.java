e3cf6da14a24d26600748f78e82a4a6b10600393
vankodg <vankodaniel97@gmail.com>
2019-02-11 07:44:00 -0800

    Added test to TrainSensorTest.java

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -5,15 +5,20 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.mockito.Mockito.*;
 
+import hu.bme.mit.train.interfaces.TrainSensor;
+import hu.bme.mit.train.sensor.TrainSensorImpl;
+
 public class TrainSensorTest {
+
+    TrainSensor sensor;
 
     @Before
     public void before() {
-        // TODO Add initializations
+        sensor = new TrainSensorImpl(null, null);
     }
 
     @Test
-    public void ThisIsAnExampleTestStub() {
-        // TODO Delete this and add test cases based on the issues
+    public void SpeedLimitOverriding() {
+        Assert.assertEquals(10, sensor.getSpeedLimit());
     }
 }
